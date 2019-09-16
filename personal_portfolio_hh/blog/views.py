cd0fbde341cd4ceb535946b2c29791e75fac881e
Jethet <henriette.hettinga@gmail.com>
2019-09-16 18:43:32 +0200

    Debugging error 'cannot resolve keyword'

--- personal_portfolio_hh/blog/views.py
+++ personal_portfolio_hh/blog/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render
-from blog.models import Post
-from .forms import CommentForm
+from blog.models import Post, Comment
+from blog.forms import CommentForm
 
 def blog_index(request):
     posts = Post.objects.all().order_by('-created_on') # most recent first
     context = {'posts': posts}
     return render(request, 'blog_index.html', context)
 
-def blog_category(request):
-    posts = Post.objects.filter(
-        categories_name_contains=category
-    ).order_by('-created_on')
+def blog_category(request, category):
+    posts = Post.objects.filter(categories_name_contains=category).order_by('-created_on')
     context = {'category': category, 'posts': posts}
     return render(request, 'blog_category.html', context)
 
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
 
     form = CommentForm()
     if request.method == 'POST':
@@ -28,6 +27,5 @@ def blog_detail(request, pk):
             )
             comment.save()
 
-    comments = Comment.objects.filter(post=post)
     context = {'post': post, 'comments': comments, 'form': form}
     return render(request, 'blog_detail.html', context)
